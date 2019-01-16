4c652ea35ea7e8bad66c0982224b86c71c4a4430
HandsomeDong <929091698@qq.com>
2019-01-16 06:40:06 -0800

    gitignore config

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,17 +2,27 @@
 
 namespace app\index\controller;
 
+use app\index\model\LoginResult;
 use app\index\service\Database;
+use app\index\service\UserParser;
+use app\index\tools\Token;
 use think\Request;
+
 
 class Index
 {
+    private $userParser;
+
+    public function __construct(UserParser $userParser)
+    {
+        $this->userParser = $userParser;
+    }
 
 
     public function index()
     {
         $testDatabase = new Database();
-        $result = $testDatabase->test1();
+        $result = 0;
         return json(['data' => $result, 'code' => 200, 'message' => 'successful']);
         //return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="ad_bd568ce7058a1091"></think>';
     }
@@ -20,7 +30,23 @@ class Index
 
     public function test()
     {
-	return 'fuckfuckfuck';	
+        $database = new Database();
+        $loginJson = new LoginResult();
+        $token = null;
+
+        $request = Request::instance();
+        $param = $request->param();
+        $data = $database->login($param);
+
+        if (count($data) == 0) {
+            $result = $loginJson->getFailedResult();
+            return $result;
+        } else {
+            $token = Token::getToken($request->param('usernum'));
+            $test = $this->userParser->parseToken($token);
+            return $test;
+        }
+
     }
 
     public function fuck()
@@ -28,6 +54,12 @@ class Index
         return json(['message' => 'Fuck your mother!!!']);
     }
 
+    /**
+     * @return \think\response\Json
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
     public function login()
     {
         $database = new Database();
---
+++ application/index/service/UserParser.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fitz
+ * Date: 1/16/19
+ * Time: 6:19 AM
+ */
+
+namespace app\index\service;
+
+
+use Firebase\JWT\JWT;
+
+class UserParser
+{
+    private $key;
+    private $alg = [];
+
+
+    public function __construct()
+    {
+        $this->key = 'YUHAIDONG';
+        $this->alg = 'HS256';
+
+    }
+
+    public function parseToken($token)
+    {
+        $data = JWT::decode($token, $this->key, $this->alg);
+        return $data;
+
+    }
+
+}
