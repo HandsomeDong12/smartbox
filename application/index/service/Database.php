6cc30782b3b690bca4b25ef23103b522d9127889
HandsomeDong <929091698@qq.com>
2019-01-29 04:45:42 -0800

    Controller

--- application/index/service/Database.php
+++ application/index/service/Database.php
@@ -66,7 +66,7 @@ class Database
 
         $userData = $user->where('userId', $userId)->find();
 
-        $medicineData = $medicine->where('cardId', $userData->cardId)->find();
+        $medicineData = $medicine->where('cardId', $userData->userData->cardId)->find();
 
         return $medicineData;
     }
